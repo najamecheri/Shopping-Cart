74befe102ebbbb10229c7410e46fa348df7ac278
NajaMecheri <najamecheri28@gmail.com>
2026-08-29 00:28:11 +0530

    final touchup

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './context/CartContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "./context/CartContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <CartProvider>
         <App />
-      </CartProvider> 
+      </CartProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
